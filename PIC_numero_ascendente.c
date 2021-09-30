da15684614e9e7a316e3b2cad799fbb7ab543cb5
foxyctg21 <cristiantg2014@hotmail.es>
2021-09-30 14:34:28 +0200

    Se inicia el proyecto

---
+++ PIC_numero_ascendente.c
@@ -0,0 +1,20 @@
+/*
+ * File:   PIC_Parpadeo.c
+ * Author: Usuario
+ *
+ * Created on 30 de septiembre de 2021, 14:26
+ */
+#include <xc.h>
+#pragma config PLLDIV = 5
+#pragma config CPUDIV = OSC1_PLL2
+#pragma config FOSC = HSPLL_HS
+#pragma config LVP = OFF
+#define _XTAL_FREQ 48000000
+
+void display(int num){
+    
+}
+
+void main(void) {
+    return;
+}
