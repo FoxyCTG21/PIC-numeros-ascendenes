5226cf61d7e4588a0d9d455312948472c81e4649
foxyctg21 <cristiantg2014@hotmail.es>
2021-10-04 13:53:06 +0200

    Se genera la libreria display y un bucle for ascendente

--- PIC_numero_ascendente.c
+++ PIC_numero_ascendente.c
@@ -1,9 +1,10 @@
 /*
  * File:   PIC_Parpadeo.c
- * Author: Usuario
+ * Author: Foxy
  *
  * Created on 30 de septiembre de 2021, 14:26
  */
+
 #include <xc.h>
 #pragma config PLLDIV = 5
 #pragma config CPUDIV = OSC1_PLL2
@@ -17,12 +18,44 @@ void display(int num){
     if(num == 0){
         LATB = 0b0111111;
     }
+    if(num == 1){
+        LATB = 0b0000110;
+    }
+    if(num == 2){
+        LATB = 0b1011011;
+    }
+    if(num == 3){
+        LATB = 0b1001111;
+    }
+    if(num == 4){
+        LATB = 0b1100110;
+    }
+    if(num == 5){
+        LATB = 0b1101101;
+    }
+    if(num == 6){
+        LATB = 0b1111101;
+    }
+    if(num == 7){
+        LATB = 0b0000111;
+    }
+    if(num == 8){
+        LATB = 0b1111111;
+    }
+    if(num == 9){
+        LATB = 0b1100111;
+    }
 }
 
 void main(void) {
     
     TRISB=0b00000000;
     
-    display(0);
+    while(1){
+        for(int num = 0; num <= 9; num++){
+            display(num);
+            __delay_ms(600);
+        }
+    }
     
 }
---
+++ build/default/production/PIC_numero_ascendente.p1.d
@@ -0,0 +1,2 @@
+build/default/production/PIC_numero_ascendente.p1:  \
+PIC_numero_ascendente.c 
